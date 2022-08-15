1f271d4409aeac28c7f1b8743e7f174f826993f6
Paddybarn <paddybarn21@gmail.com>
2022-08-15 13:17:15 -0400

    added fetch call for weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//golf course fetch call for Columbus coordinates within 10 mile radius
+// golf course fetch call for Columbus coordinates within 10 mile radius
 const options = {
 	method: 'GET',
 	headers: {
@@ -11,3 +11,21 @@ fetch('https://golf-course-finder.p.rapidapi.com/courses?radius=10&lat=39.983334
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+//Columbus weather fetch call
+function getWeatherApi() {
+
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=Columbus&appid=01c6acda042379425ee30a68789c29c5';
+  
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      })
+    }
+
+    
+getWeatherApi()
+
+
