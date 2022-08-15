55895ebea129eab49caff7950233628c739f3320
Paddybarn <paddybarn21@gmail.com>
2022-08-15 14:03:22 -0400

    added project requirements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,28 @@
+// Use a CSS framework other than Bootstrap.
+
+// Be deployed to GitHub Pages.
+
+// Be interactive (i.e: accept and respond to user input).
+
+// Use at least two server-side APIs.
+
+// Does not use alerts, confirms, or prompts (use modals).
+
+// Use client-side storage to store persistent data.
+
+// Be responsive.
+
+// Have a polished UI.
+
+// Have a clean repository that meets quality coding standards (file structure, naming conventions, follows best practices for class/id-naming conventions, indentation, quality comments, etc.).
+
+// Have a quality README (with unique name, description, technologies used, screenshot, and link to deployed application).
+
+// Finally, You must add your project to the portfolio that you created in Module 2.
+
+
+
+
 // golf course fetch call for Columbus coordinates within 10 mile radius
 const options = {
 	method: 'GET',
@@ -31,5 +56,5 @@ getWeatherApi()
 
     
 $('.locationBtn').click( "click", function() {
-    $('.modal-content').modal('show');
+    $('.modal').modal('show');
 });
