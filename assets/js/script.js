e6595d1a98d2454d4185f2854659e4a25c6a6558
kdicari24@gmail.com <kdicari24@gmail.com>
2022-08-18 13:41:37 -0400

    saved changes for push

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,31 @@
-
-// Use a CSS framework other than Bootstrap.
-
-// Be deployed to GitHub Pages.
-
-// Be interactive (i.e: accept and respond to user input).
-
-// Use at least two server-side APIs.
-
-// Does not use alerts, confirms, or prompts (use modals).
-
-// Use client-side storage to store persistent data.
-
-// Be responsive.
-
-// Have a polished UI.
-
-// Have a clean repository that meets quality coding standards (file structure, naming conventions, follows best practices for class/id-naming conventions, indentation, quality comments, etc.).
-
-// Have a quality README (with unique name, description, technologies used, screenshot, and link to deployed application).
 // Finally, You must add your project to the portfolio that you created in Module 2.
 
-// golf course fetch call for Columbus coordinates within 10 mile radius
 var fetchbutton = document.querySelectorAll(".btn btn-primary")
 var searchContainer = document.getElementById("results")
-
-
 var locationEl = document.getElementById("location")
-
 var tempEl = document.getElementById("temp")
 var skyTextEl = document.getElementById("weatherText")
 var exampleZipCode = document.getElementById("exampleZipCode")
 var windEl = document.getElementById("wind")
 
-function renderLastData() {
-    var cityData = localStorage.getItem("savedCity");
-    var tempData = localStorage.getItem("savedTemp");
-    var skyData = localStorage.getItem("savedSky");
-    var windData = localStorage.getItem("savedWind");
 
-    locationEl.textContent = ("City:   " + cityData);
-    tempEl.textContent = ("Temperature:   " + tempData + "°F");
-    skyTextEl.textContent = ("Weather:   " + skyData);
-    windEl.textContent = ("Wind Speed:   " + windData + "mph");
-}
+function golfDetailsApi() {
+        const options = {
+                method: 'GET',
+                headers: {
+            'X-RapidAPI-Key': '804debc86amsh91473b414b0f4a2p1cd0cfjsncf3998336eab',
+            'X-RapidAPI-Host': 'golf-course-finder.p.rapidapi.com'
+        }
 
-// function golfDetailsApi() {
-//     const options = {
-//         method: 'GET',
-//         headers: {
-//             'X-RapidAPI-Key': '804debc86amsh91473b414b0f4a2p1cd0cfjsncf3998336eab',
-//             'X-RapidAPI-Host': 'golf-course-finder.p.rapidapi.com'
-//         }
-
-//     };
+    };
     
-//     fetch('https://golf-course-finder.p.rapidapi.com/course/details?zip=93953&name=Pebble%20Beach%20Golf%20Links', options)
-//         .then(response => response.json())
-//         .then(response => console.log(response))
-//         .catch(err => console.error(err));
-//     }
+    fetch('https://golf-course-finder.p.rapidapi.com/course/details?zip=93953&name=Pebble%20Beach%20Golf%20Links', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+    }
 
+// golf course fetch call for Columbus coordinates within x mile radius
 const golfApi = function (params) {
     const options = {
         method: 'GET',
@@ -68,8 +34,8 @@ const golfApi = function (params) {
             'X-RapidAPI-Host': 'golf-course-finder.p.rapidapi.com'
         }
     }
-fetch(`https://golf-course-finder.p.rapidapi.com/courses?radius=${params.radius}&lat=${params.lat}&lng=${params.long}`, options)
-.then(function (response) {
+    fetch(`https://golf-course-finder.p.rapidapi.com/courses?radius=${params.radius}&lat=${params.lat}&lng=${params.long}`, options)
+    .then(function (response) {
     return response.json()
 })
 .then(function (data) {
@@ -77,27 +43,27 @@ fetch(`https://golf-course-finder.p.rapidapi.com/courses?radius=${params.radius}
         console.log(data)
         for (var i=0; i < data.courses.length; i++){
             console.log(data.courses[i].name)
-
-        var courseName = document.createElement('li');
-        courseName.textContent = data.courses[i].name+" - "+ data.courses[i].distance + " miles away";
-        console.log(courseName)
-        searchContainer.append(courseName)
+            
+            var courseName = document.createElement('li');
+            courseName.textContent = data.courses[i].name+" - "+ data.courses[i].distance + " miles away";
+            console.log(courseName)
+            searchContainer.append(courseName)
         }}
     })
 }
 
 
 const getLongLat = function(zipCode, radius){
-        var long = ""
-        var lat = ""
-        const apiKey = "113605930247714900398x83872"
-        console.log(`passed zip code = ${zipCode}`)
-        console.log(`passed radius = ${radius}`)
-fetch(`https://geocode.xyz/+${zipCode}+?json=1&auth=${apiKey}`) 
+    var long = ""
+    var lat = ""
+    const apiKey = "113605930247714900398x83872"
+    console.log(`passed zip code = ${zipCode}`)
+    console.log(`passed radius = ${radius}`)
+    fetch(`https://geocode.xyz/+${zipCode}+?json=1&auth=${apiKey}`) 
     .then(function(response){
         console.log(response)
         return response.json()
-
+        
     })
     .then(function(data){
         console.log(data)
@@ -113,32 +79,46 @@ fetch(`https://geocode.xyz/+${zipCode}+?json=1&auth=${apiKey}`)
         }
         console.log(params)
 
-    golfApi(params)
+        golfApi(params)
     })
 }
 
+function renderLastData() {
+    var cityData = localStorage.getItem("savedCity");
+    var tempData = localStorage.getItem("savedTemp");
+    var skyData = localStorage.getItem("savedSky");
+    var windData = localStorage.getItem("savedWind");
+    
+    locationEl.textContent = ("City: " + cityData);
+    tempEl.textContent = ("Temperature: " + tempData + "°F");
+    skyTextEl.textContent = ("Weather: " + skyData);
+    windEl.textContent = ("Wind Speed: " + windData + "mph");
+}
 
 //Columbus weather fetch call
 function getWeatherApi(zipCode) {
     console.log("my past data is " + zipCode)
     let param = zipCode
     let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?zip=${param},us&units=imperial&appid=01c6acda042379425ee30a68789c29c5`;
-    //{curly ${var} brackets for user input on zip code}
     
     fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            var wCity = data.city.name
-            console.log(wCity)
-            locationEl.append(wCity)
-
-            var wTemp = parseInt(data.list[0].main.temp)
-            tempEl.append(wTemp + "°F")
-
-            var wSky = data.list[0].weather[0].main
-            skyTextEl.append(wSky)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        var wCity = data.city.name
+        localStorage.setItem("savedCity", wCity)
+        
+        var wTemp = data.list[0].main.temp
+        localStorage.setItem("savedTemp", wTemp)
+        
+        var wSky = data.list[0].weather[0].main
+        localStorage.setItem("savedSky", wSky)
+            
+        var wWind = data.list[0].wind.speed
+        localStorage.setItem("savedWind", wWind)
+            
+        renderLastData()
         })
 }
 
